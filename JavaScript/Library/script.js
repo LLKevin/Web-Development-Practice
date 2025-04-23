bcbf11f254cfd5d62778d72dc78c821be92a4ed9
LLKevin <kevinleo.lucas@gmail.com>
2025-04-22 23:59:27 -0400

    Complete ChangeStatus and Delete Button

--- JavaScript/Library/script.js
+++ JavaScript/Library/script.js
@@ -1,4 +1,11 @@
-const myLibrary = [];
+let myLibrary = [];
+let modelBtn = document.getElementsByClassName('modelBtn').item(0);
+let dialog = document.querySelector('dialog');
+let bookForm = document.getElementsByClassName('bookForm');
+
+modelBtn.addEventListener("click", ()=>{
+    dialog.showModal();
+});
 
 function book(id, title, author, pageNumbers, haveRead){
     this.id = id;
@@ -19,34 +26,63 @@ function addBookToLibrary(title, author, pageNumbers, haveRead){
 (function populateBook(){
     addBookToLibrary("The Hobbit", "J.R.R Tolkien", 285, false);
     addBookToLibrary("Cell", "Stephen King", 449, true);
+    addBookToLibrary("Twilight", "Stephanie Meyer", 350, false);
+    addBookToLibrary("Twilight", "Stephanie Meyer", 350, false);
+
 })();
 
 function displayBooks(){
     const bookListDomEle = document.getElementsByClassName("childcontainercontent")
     myLibrary.forEach(element => {
-        let headerElement = createButtons()
-        console.log(headerElement.outerHTML)
         let libraryInfo = document.createElement("div");
         libraryInfo.className = "libraryBookInfo";
-        libraryInfo.innerHTML = `${createButtons().outerHTML} </br><p>${element.title}</p><p>${element.author}</p></br><p>${element.pageNumbers}</p></br><p>${element.haveRead}</p></br>`
+        // The event listener going to the parent event and filtered after.
+        libraryInfo.addEventListener("click", (e) =>{
+            let targetElementAttribute = e.target.parentNode.parentElement.dataset.attribute;
+            if(e.target.className === "InfoDelete"){
+                e.target.parentNode.parentElement.parentElement.removeChild(e.target.parentNode.parentElement)
+                myLibrary = myLibrary.filter((item) => item.id != targetElementAttribute); 
+            }
+            if(e.target.className === "changeStatus"){
+                let statusElement = e.target.parentElement.parentElement.childNodes[5].textContent;
+                let currentStatus =  statusElement === "true";
+                let newStatusBool = !currentStatus;
+
+                //The default value will be fault if the page is refreshed.
+                myLibrary.find(item => item.id === targetElementAttribute).haveRead = newStatusBool;
+                console.log(myLibrary)
+
+
+                e.target.parentElement.parentElement.childNodes.forEach((x) =>{
+                    if(x.className === "haveRead"){
+                        x.textContent = newStatusBool;
+                    }
+                });
+            }
+        });
+
+        libraryInfo.setAttribute(`data-attribute`, element.id);
+        libraryInfo.innerHTML = `${createButtons().outerHTML} <p class='title'>${element.title}</p><p class='author'>${element.author}</p><p class='pageNumbers'>${element.pageNumbers}</p><p class='haveRead'>${element.haveRead}</p>`
         bookListDomEle.item(0).appendChild(libraryInfo);
     });
-    
 }
 
 function createButtons(){
     let libraryInfoEdit = document.createElement("button");
-    libraryInfoEdit.value = "Edit";
-    libraryInfoEdit.textContent = "Edit";
+    libraryInfoEdit.value = "changeStatus";
+    libraryInfoEdit.textContent = "Change Status";
+    libraryInfoEdit.className = "changeStatus";
+
     let libraryInfoDelete = document.createElement("button");
     libraryInfoDelete.value = "Delete";
     libraryInfoDelete.textContent = "Delete";
+    libraryInfoDelete.className = "InfoDelete";
+  
     let headerContainer = document.createElement("div")
     headerContainer.className = "cardHeader";
     headerContainer.appendChild(libraryInfoEdit);
     headerContainer.appendChild(libraryInfoDelete);
     return headerContainer;
 }
-
 
 displayBooks();
