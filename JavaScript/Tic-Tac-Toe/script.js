bde8ef861555c4c2a4556277975aa861528bfa78
LLKevin <kevinleo.lucas@gmail.com>
2025-05-03 16:02:37 -0400

    Add UiController to draw the UI

--- JavaScript/Tic-Tac-Toe/script.js
+++ JavaScript/Tic-Tac-Toe/script.js
@@ -1,10 +1,20 @@
+
 const GameBoard = (function(){
+    let rowColumn = 3;
     let gameboard = [];
     //populate the blank board
-    const getGameBoard = () => gameBoard;
-    
+    const getGameBoard = () => gameboard;
+    for(let i =0; i < rowColumn; i++){
+        gameboard[i] = [];
+        for(let k = 0; k < rowColumn; k++){
+            gameboard[i].push(Cell());
+        }
+    }
+
+
+   // console.log(gameboard)
     function Cell(){
-        let value = 0;
+        let value = "";
         const markCell = (player) => {
             value = player;
             // allow the respective play to mark the game board.
@@ -17,10 +27,14 @@ const GameBoard = (function(){
             getValue
         };
     }
+
+    return {getGameBoard};
 })();
 
-function gameController(playerOneName="Player One", playerTwoName="Player Two" ){
-    const board = GameBoard();
+// used to control the game
+function GameController(playerOneName="Player One", playerTwoName="Player Two" ){
+    const board = GameBoard.getGameBoard();
+    console.log(board);
     const activePlayer = players[0];
     const players = [{
         name:playerOneName,
@@ -35,5 +49,33 @@ function gameController(playerOneName="Player One", playerTwoName="Player Two" )
     }
     const getActivePlayer = () => activePlayer;
 
-    const playARound = (column) => {}
+    const playARound = (column) => {
+        // Will mark the cell() and check if a cell can be marked.
+        // check win condition
+    }
 } 
+
+function UiController(){
+    const board = GameBoard.getGameBoard();
+    const buttonDiv = document.createElement("div");
+    buttonDiv.className = "buttonDiv";
+    const divContainerRef = document.getElementsByClassName('board');
+    const generateButton = () => document.createElement('button');
+    function GenerateBoard(){
+        for(let i =0; i < board.length; i++){
+            for(k=0; k<board.length; k++){
+                let button = generateButton();
+                button.value = board[i][k].getValue() === 1 ? "X" : "O";                
+                button.textContent = board[i][k].getValue();
+                buttonDiv.appendChild(button);
+            }
+        }
+        divContainerRef[0].innerHTML = buttonDiv.outerHTML;
+    }
+
+   const displayBoard = () => new GenerateBoard();
+
+   displayBoard();
+}
+
+UiController();
